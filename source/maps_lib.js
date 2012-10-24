0cd0bb77f64fd950157d5ccf23a42e81a4a3d2e3
Juan-Pablo Velez <jpvelez@gmail.com>
2012-10-23 22:23:03 -0500

    built the map. first commit

---
+++ source/maps_lib.js
@@ -0,0 +1,224 @@
+/*!
+ * Searchable Map Template with Google Fusion Tables
+ * http://derekeder.com/searchable_map_template/
+ *
+ * Copyright 2012, Derek Eder
+ * Licensed under the MIT license.
+ * https://github.com/derekeder/FusionTable-Map-Template/wiki/License
+ *
+ * Date: 8/15/2012
+ * 
+ */
+ 
+var MapsLib = MapsLib || {};
+var MapsLib = {
+  
+  //Setup section - put your Fusion Table details here
+  //Using the v1 Fusion Tables API. See https://developers.google.com/fusiontables/docs/v1/migration_guide for more info
+  
+  //the encrypted Table ID of your Fusion Table (found under File => About)
+  //NOTE: numeric IDs will be depricated soon
+  fusionTableId:      "1MkEpGm9biof-EWTWeOPYrjoLilL7eQoDPx3hdOg",  
+  
+  //*New Fusion Tables Requirement* API key. found at https://code.google.com/apis/console/   
+  //*Important* this key is for demonstration purposes. please register your own.   
+  googleApiKey:       "AIzaSyCoB4JKY0yWBYDkZHnHs582dChPGdnXkh0",        
+  
+  //name of the location column in your Fusion Table. 
+  //NOTE: if your location column name has spaces in it, surround it with single quotes 
+  //example: locationColumn:     "'my location'",
+  locationColumn:     "geometry",  
+
+  map_centroid:       new google.maps.LatLng(41.891405,-87.626571), //center that your map defaults to
+  locationScope:      "chicago",      //geographical area appended to all address searches
+  recordName:         "Precinct",       //for showing number of results
+  recordNamePlural:   "results", 
+  
+  searchRadius:       1,            //in meters ~ 1/2 mile
+  defaultZoom:        11,             //zoom level when map is loaded (bigger is more zoomed in)
+  addrMarkerImage: 'http://derekeder.com/images/icons/blue-pushpin.png',
+  currentPinpoint: null,
+  
+  initialize: function() {
+    $( "#resultCount" ).html("");
+  
+    geocoder = new google.maps.Geocoder();
+    var myOptions = {
+      zoom: MapsLib.defaultZoom,
+      center: MapsLib.map_centroid,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map($("#mapCanvas")[0],myOptions);
+    
+    MapsLib.searchrecords = null;
+    
+    //reset filters
+    $("#txtSearchAddress").val(MapsLib.convertToPlainString($.address.parameter('address')));
+    $(":checkbox").attr("checked", "checked");
+    $("#resultCount").hide();
+     
+    //run the default search
+    MapsLib.doSearch();
+  },
+  
+  doSearch: function(location) {
+    MapsLib.clearSearch();
+    var address = $("#txtSearchAddress").val();
+
+    var whereClause = MapsLib.locationColumn + " not equal to ''";
+    
+    
+    if (address != "") {
+      if (address.toLowerCase().indexOf(MapsLib.locationScope) == -1)
+        address = address + " " + MapsLib.locationScope;
+  
+      geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          MapsLib.currentPinpoint = results[0].geometry.location;
+          
+          $.address.parameter('address', encodeURIComponent(address));
+          map.setCenter(MapsLib.currentPinpoint);
+          map.setZoom(14);
+          
+          MapsLib.addrMarker = new google.maps.Marker({
+            position: MapsLib.currentPinpoint, 
+            map: map, 
+            icon: MapsLib.addrMarkerImage,
+            animation: google.maps.Animation.DROP,
+            title:address
+          });
+          
+          whereClause += " AND ST_INTERSECTS(" + MapsLib.locationColumn + ", CIRCLE(LATLNG" + MapsLib.currentPinpoint.toString() + "," + MapsLib.searchRadius + "))";
+          
+          MapsLib.submitSearch(whereClause, map, MapsLib.currentPinpoint);
+        } 
+        else {
+          alert("We could not find your address: " + status);
+        }
+      });
+    }
+    else { //search without geocoding callback
+      MapsLib.submitSearch(whereClause, map);
+    }
+  },
+  
+  submitSearch: function(whereClause, map, location) {
+    //get using all filters
+    MapsLib.searchrecords = new google.maps.FusionTablesLayer({
+      query: {
+        from:   MapsLib.fusionTableId,
+        select: MapsLib.locationColumn,
+        where:  whereClause
+      }
+    });
+    MapsLib.searchrecords.setMap(map);
+    MapsLib.displayCount(whereClause);
+  },
+  
+  clearSearch: function() {
+    if (MapsLib.searchrecords != null)
+      MapsLib.searchrecords.setMap(null);
+    if (MapsLib.addrMarker != null)
+      MapsLib.addrMarker.setMap(null);  
+    if (MapsLib.searchRadiusCircle != null)
+      MapsLib.searchRadiusCircle.setMap(null);
+  },
+  
+  findMe: function() {
+    // Try W3C Geolocation (Preferred)
+    var foundLocation;
+    
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        foundLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+        MapsLib.addrFromLatLng(foundLocation);
+      }, null);
+    }
+    else {
+      alert("Sorry, we could not find your location.");
+    }
+  },
+  
+  addrFromLatLng: function(latLngPoint) {
+    geocoder.geocode({'latLng': latLngPoint}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        if (results[1]) {
+          $('#txtSearchAddress').val(results[1].formatted_address);
+          $('.hint').focus();
+          MapsLib.doSearch();
+        }
+      } else {
+        alert("Geocoder failed due to: " + status);
+      }
+    });
+  },
+  
+  drawSearchRadiusCircle: function(point) {
+      var circleOptions = {
+        strokeColor: "#4b58a6",
+        strokeOpacity: 0.3,
+        strokeWeight: 1,
+        fillColor: "#4b58a6",
+        fillOpacity: 0.05,
+        map: map,
+        center: point,
+        clickable: false,
+        zIndex: -1,
+        radius: parseInt(MapsLib.searchRadius)
+      };
+      MapsLib.searchRadiusCircle = new google.maps.Circle(circleOptions);
+  },
+  
+  query: function(selectColumns, whereClause, callback) {
+    var queryStr = [];
+    queryStr.push("SELECT " + selectColumns);
+    queryStr.push(" FROM " + MapsLib.fusionTableId);
+    queryStr.push(" WHERE " + whereClause);
+  
+    var sql = encodeURIComponent(queryStr.join(" "));
+    $.ajax({url: "https://www.googleapis.com/fusiontables/v1/query?sql="+sql+"&callback="+callback+"&key="+MapsLib.googleApiKey, dataType: "jsonp"});
+  },
+
+  handleError: function(json) {
+    if (json["error"] != undefined)
+      console.log("Error in Fusion Table call: " + json["error"]["message"]);
+  },
+  
+  displayCount: function(whereClause) {
+    var selectColumns = "Count()";
+    MapsLib.query(selectColumns, whereClause,"MapsLib.displaySearchCount");
+  },
+  
+  displaySearchCount: function(json) { 
+    MapsLib.handleError(json);
+    var numRows = 0;
+    if (json["rows"] != null)
+      numRows = json["rows"][0];
+    
+    var name = MapsLib.recordNamePlural;
+    if (numRows == 1)
+    name = MapsLib.recordName;
+    $( "#resultCount" ).fadeOut(function() {
+        $( "#resultCount" ).html(MapsLib.addCommas(numRows) + " " + name + " found");
+      });
+    $( "#resultCount" ).fadeIn();
+  },
+  
+  addCommas: function(nStr) {
+    nStr += '';
+    x = nStr.split('.');
+    x1 = x[0];
+    x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+      x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+    return x1 + x2;
+  },
+  
+  //converts a slug or query string in to readable text
+  convertToPlainString: function(text) {
+    if (text == undefined) return '';
+  	return decodeURIComponent(text);
+  }
+}
